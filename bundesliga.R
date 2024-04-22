081561e82317f32b713cb31f63fb035c939c81a9
GavDotZip <gavdotzip@gmail.com>
2024-04-22 23:56:14 +0100

    commit

--- bundesliga.R
+++ bundesliga.R
@@ -1,2 +1,10 @@
 # Dataset taken from https://www.kaggle.com/datasets/oles04/bundesliga-soccer-player
 
+# Load the required library for data manipulation
+library(dplyr)
+
+# Read the CSV file into a dataframe
+bliga <- read.csv("C:/Users/gavin/RProjects/Sports-Analysis/datasets/bundesliga_players.csv")
+
+# View the structure of the dataframe
+str(bliga)
