c5e235e398c0a340a3136b2dfcb6e364e5791920
GavDotZip <gavdotzip@gmail.com>
2024-04-26 23:26:59 +0100

    add MV

--- bundesliga.R
+++ bundesliga.R
@@ -12,3 +12,18 @@ str(bliga)
 # Display summary of data
 cat("\nSummary of Data:\n")
 print(summary(bliga))
+
+# See Which Players had the Highest Market Value
+# Grouped by Name, get the Sum Total of Max_Price
+max_marketVal <- bliga %>%
+  group_by(name) %>%
+  summarise(MaxValue = sum(max_price))
+
+# Find the Top 10 Most Expensive Players
+top10MV <- max_marketVal %>%
+  arrange(desc(MaxValue)) %>%
+  head(10)
+
+# Print the Top 10 Most Expensive Players
+cat("\nTop 10 Players by Market value (Millions):\n")
+print(top10MV)
